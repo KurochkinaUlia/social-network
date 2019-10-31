5bf2f65ef48a2b6939a95d92e15fb80a5bef9e83
pavel <pavel.1c@mail.ru>
2019-10-31 16:15:17 +0300

    1

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className="general">
+        <img className="logo"
+             src='https://www.clipartmax.com/png/full/270-2703019_pictures-gallery-of-company-logos-design-free-logo-design-3d-png.png'
+             alt="logo"/>
+
+    </header>
+}
+
+export default Header;
