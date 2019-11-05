700a303a2fe8d6e777305873e2917e36be1ce379
KurochkinaUlia <kurochkina.1@yandex.ru>
2019-11-06 00:04:17 +0300

    Добавила заставку и профайл. Начала эксп-ть с module

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
-import './App.css';
+import './App.css' ;
 import './Header.css';
 import Header from "./Header";
 import './Menu.css';
 import Menu from "./Menu";
+import './Profile.css';
+import Profile from "./Profile";
 
 const App = () => {
     return (
         <div className="app-wrapper">
-        <Header/>
-        <Menu />
+            <Header/>
+            <div className="profile">
+                <Menu/>
+                <Profile/>
+            </div>
         </div>
     )
 }
